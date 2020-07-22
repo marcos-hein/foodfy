080505e0e736f6efd6be444729d27cd1e49605f7
Vinicius Hein <vinicius.hein@gmail.com>
2020-07-21 21:10:35 -0300

    starting view engine

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
+
+// importar dependências do express
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
+server.set("view engine", "html")
 
-server.get("/", function(req, res) {
-    return res.send("hi")
+server.use(express.static("public"))
+
+nunjucks.configure("views", {
+    express: server
 })
 
+
+// cria a primeira rota
+server.get("/", function(req, res) {
+    return res.render("index")
+})
+
+server.get("/about-foodfy", function(req, res) {
+    return res.render("about-foodfy")
+})
+
+server.get("/recipes", function(req, res) {
+    return res.render("recipes")
+})
+
+// cria servidor
 server.listen(5000, function() {
     console.log("Server is running!")
 })
