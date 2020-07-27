31387a0cc782e634385ce6e80e18b2cb87fe6a4f
Vinicius Hein <vinicius.hein@gmail.com>
2020-07-27 07:01:51 -0300

    implementing indexOf

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,9 @@
-const modalOverlay = document.querySelector(".modal-overlay")
-const modalContent = document.querySelector(".modal-content")
 const cards = document.querySelectorAll(".card")
 
 for (const card of cards) {
     card.addEventListener("click", function() {
-        modalOverlay.classList.add("active")
-        var cardModal = card.innerHTML
-        modalContent.innerHTML = cardModal
+        const cardIndex = cards.indexOf()
+        console.log(cards.indexOf(card))
+        window.location.href = 'recipes/${ cardIndex }'
     })
 }
-
-document.querySelector(".close-modal").addEventListener("click", function() {
-    modalOverlay.classList.remove("active")
-})
