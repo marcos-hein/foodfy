b0a3666324b651f76a050f6fe8e2f1c7729f488f
Vinicius Hein <vinicius.hein@gmail.com>
2020-07-31 00:28:10 -0300

    show and hide action added

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,25 @@
 const cards = document.querySelectorAll(".card")
+const hideShowButtons = document.querySelectorAll(".button")
+const recipeInfos = document.querySelectorAll(".recipe-hide-show")
 
 for (const card of cards) {
     card.addEventListener("click", function() {
         const recipeId = card.getAttribute('id')
         
         window.location.href = `recipes/${ recipeId }`
+    })
+}
+
+for (let i = 0; i < hideShowButtons.length; i++) {
+    hideShowButtons[i].addEventListener("click", function() {
+        if(recipeInfos[i].classList.contains('hidden')) {
+            recipeInfos[i].classList.remove('hidden')
+
+            hideShowButtons[i].textContent = 'ESCONDER'
+        } else {
+            recipeInfos[i].classList.add('hidden')
+            
+            hideShowButtons[i].textContent = 'MOSTRAR'
+        }
     })
 }
