c1d42aa6041f79267dd8fb35acd119b974edd53e
Vinicius Hein <vinicius.hein@gmail.com>
2020-09-16 00:00:32 -0300

    script updated

--- public/scripts.js
+++ public/scripts.js
@@ -10,16 +10,30 @@ for (const card of cards) {
     })
 }
 
-for (let i = 0; i < hideShowButtons.length; i++) {
-    hideShowButtons[i].addEventListener("click", function() {
-        if(recipeInfos[i].classList.contains('hidden')) {
-            recipeInfos[i].classList.remove('hidden')
+// for (let i = 0; i < hideShowButtons.length; i++) {
+//     hideShowButtons[i].addEventListener("click", function() {
+//         if(recipeInfos[i].classList.contains('hidden')) {
+//             recipeInfos[i].classList.remove('hidden')
 
-            hideShowButtons[i].textContent = 'ESCONDER'
-        } else {
-            recipeInfos[i].classList.add('hidden')
+//             hideShowButtons[i].textContent = 'ESCONDER'
+//         } else {
+//             recipeInfos[i].classList.add('hidden')
             
-            hideShowButtons[i].textContent = 'MOSTRAR'
+//             hideShowButtons[i].textContent = 'MOSTRAR'
+//         }
+//     })
+// }
+
+for (const button in hideShowButtons) {
+    hideShowButtons[button].addEventListener("click", function() {
+        if(recipeInfos[button].classList.contains('hidden')) {
+            recipeInfos[button].classList.remove('hidden')
+
+            hideShowButtons[button].textContent = 'ESCONDER'
+        } else {
+            recipeInfos[button].classList.add('hidden')
+            
+            hideShowButtons[button].textContent = 'MOSTRAR'
         }
     })
 }
