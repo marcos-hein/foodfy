2186a4cc9f4fb8c18a24f486ace7db7ffc18cc5f
Vinicius Hein <vinicius.hein@gmail.com>
2020-12-26 23:50:10 -0300

    creating, listing, editing and deleting chefs

---
+++ src/app/controllers/ChefsController.js
@@ -0,0 +1,188 @@
+const { date } = require("../../lib/utils")
+const Chef = require("../models/Chef")
+// function removeLastEmptyElement (array) {
+//     if (array[array.length - 1] == "") {
+//         array.pop()
+//     }
+//     return array
+// }
+
+module.exports = {
+    index(req, res) {
+        Chef.all(function(chefs) {
+            return res.render("admin/chefs/index", { chefs })
+        })
+    },    
+    create(req, res) {
+        return res.render("../views/admin/chefs/create")
+    },
+    post(req, res) {
+    
+        const keys = Object.keys(req.body)
+    
+        for(const key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, Fill all fields!')
+            }
+        }
+
+        Chef.create(req.body, function(chef) {
+            return res.redirect(`/admin/chefs/${chef.id}`)
+        })
+    },
+    show(req, res) {
+        Chef.find(req.params.id, function(chef) {
+            if (!chef) return res.send("Chef not found!")
+
+            return res.render("admin/chefs/detail-chef", { chef })
+        })
+    },
+    edit(req, res) {
+        Chef.find(req.params.id, function(chef) {
+            if (!chef) return res.send("Chef not found!")
+
+            return res.render("admin/chefs/edit", { chef })
+        })
+    },
+    put(req, res) {
+        const keys = Object.keys(req.body)
+    
+        for(const key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, Fill all fields!')
+            }
+        }
+    
+        Chef.update(req.body, () => {
+            return res.redirect(`/admin/chefs/${req.body.id}`)
+        })
+
+    },
+    delete(req, res) {
+        Chef.delete(req.body.id, () => {
+            return res.redirect("/admin/chefs")
+        })
+    }
+}
+
+// exports.index = function(req, res) {
+//     const chefs = data.chefs
+//     return res.render("../views/admin/chefs/index", { chefs })
+// }
+
+// // create
+// exports.create = function(req, res) {
+//     return res.render("../views/admin/chefs/create")
+// }
+
+// // post
+// exports.post = function(req, res) {
+
+//     const keys = Object.keys(req.body)
+
+//     for(const key of keys) {
+//         if (req.body[key] == "") {
+//             return res.send('Please, Fill all fields!')
+//         }
+//     }
+
+//     let { image, title, author, ingredients, preparation, information} = req.body
+
+//     title = "receita genérica"
+//     author = "vini"
+//     ingredients = removeLastEmptyElement(ingredients)
+//     preparation = removeLastEmptyElement(preparation)
+//     const id = Number(data.chefs.length + 1)
+
+//     data.chefs.push({
+//         id,
+//         image,
+//         title,
+//         author,
+//         ingredients,
+//         preparation,
+//         information
+//     })
+
+//     fs.writeFile("data.json", JSON.stringify(data, null, 4), function(err) {
+//         if (err) return res.send('Write file error')
+
+//         return res.redirect('/admin/chefs')
+
+//     })
+// }
+
+// // show
+// exports.show = function(req, res) {
+//     const { id } = req.params
+
+//     const foundChef = data.chefs.find(function(chef) {
+//         return chef.id == id
+//     })
+
+//     if (!foundChef) return res.send('Chef not found!')
+
+//     return res.render("../views/admin/chefs/detail-chef.njk", { chef : foundChef })
+// }
+
+// // edit
+// exports.edit = function(req, res) {
+//     const { id } = req.params
+
+//     const foundChef = data.chefs.find(function(chef) {
+//         return chef.id == id
+//     })
+
+//     if (!foundChef) return res.send('Chef not found!')
+    
+//     return res.render("../views/admin/chefs/edit", { chef : foundChef })
+// }
+
+// // put
+// exports.put = function(req, res) {
+//     const { id } = req.body
+
+//     let index = 0
+
+//     const foundChef = data.chefs.find(function(chef, foundIndex) {
+//         if (id == chef.id) {
+//             index = foundIndex
+//             return true
+//         }
+//     })
+
+//     if (!foundChef) return res.send('Chef not found!')
+
+//     foundChef.ingredients = removeLastEmptyElement(foundChef.ingredients)
+//     foundChef.preparation = removeLastEmptyElement(foundChef.preparation)
+
+//     const chef = {
+//         ...foundChef,
+//         ...req.body
+//     }
+    
+//     data.chefs[index] = chef
+
+//     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+//         if (err) return res.send('Write error!')
+
+//         return res.redirect(`/admin/chefs/${id}`)
+//     })
+// }
+
+// // delete
+// exports.delete = function(req, res) {
+//     const { id } = req.body
+
+//     const filteredChefs = data.chefs.filter(function(chef) {
+//         return chef.id != id
+//     })
+
+//     data.chefs = filteredChefs
+
+//     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+//         if(err) return res.send("Write file error!")
+
+//         return res.redirect("/admin/chefs")
+//     })
+// }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = express.Router()
 const foodfy = require('./app/controllers/FoodfyController')
 const recipes = require('./app/controllers/RecipesController')
+const chefs = require('./app/controllers/ChefsController')
 
 // Rotas da página
 routes.get('/', foodfy.index)
@@ -10,7 +11,8 @@ routes.get('/recipes', foodfy.recipes)
 routes.get('/recipes/:id', foodfy.recipeDetails)
 
 
-// Rotas do admin
+// Rotas admin
+// Rotas recipes
 routes.get('/admin/recipes', recipes.index)
 routes.get('/admin/recipes/create', recipes.create)
 routes.get('/admin/recipes/:id', recipes.show)
@@ -19,6 +21,16 @@ routes.get('/admin/recipes/:id/edit', recipes.edit)
 routes.post('/admin/recipes', recipes.post)
 routes.put('/admin/recipes/', recipes.put)
 routes.delete('/admin/recipes', recipes.delete)
+
+// Rotas chefs
+routes.get('/admin/chefs', chefs.index)
+routes.get('/admin/chefs/create', chefs.create)
+routes.get('/admin/chefs/:id', chefs.show)
+routes.get('/admin/chefs/:id/edit', chefs.edit)
+
+routes.post('/admin/chefs', chefs.post)
+routes.put('/admin/chefs/', chefs.put)
+routes.delete('/admin/chefs', chefs.delete)
 
 
 module.exports = routes
